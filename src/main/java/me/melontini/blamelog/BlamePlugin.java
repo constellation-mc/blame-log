5cc5df9bf5cc1bbe13954f2168c94ccd11d3c6cb
melontini <104443436+melontini@users.noreply.github.com>
2023-12-16 19:59:08 +0700

    Minor cleanup

--- src/main/java/me/melontini/blamelog/BlamePlugin.java
+++ src/main/java/me/melontini/blamelog/BlamePlugin.java
@@ -110,38 +110,11 @@ public class BlamePlugin implements IMixinConfigPlugin {
         }
     }
 
-    @Override
-    public void onLoad(String mixinPackage) {
-
-    }
-
-    @Override
-    public String getRefMapperConfig() {
-        return null;
-    }
-
-    @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return false;
-    }
-
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-
-    }
-
-    @Override
-    public List<String> getMixins() {
-        return null;
-    }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
-    }
+    @Override public void onLoad(String mixinPackage) {}
+    @Override public String getRefMapperConfig() {return null;}
+    @Override public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {return false;}
+    @Override public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+    @Override public List<String> getMixins() {return null;}
+    @Override public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+    @Override public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
 }
