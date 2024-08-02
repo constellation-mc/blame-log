4cd61c1e2ae7014e4762638646c65d70f1d73199
melontini <104443436+melontini@users.noreply.github.com>
2024-08-02 23:20:45 +0700

    Oh, and one more thing... Spotless!

--- src/main/java/me/melontini/blamelog/BlamePlugin.java
+++ src/main/java/me/melontini/blamelog/BlamePlugin.java
@@ -1,5 +1,17 @@
 package me.melontini.blamelog;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.ProtectionDomain;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import me.melontini.dark_matter.api.base.reflect.Reflect;
 import me.melontini.dark_matter.api.base.reflect.UnsafeUtils;
 import me.melontini.dark_matter.api.base.util.MakeSure;
@@ -14,109 +26,138 @@ import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.security.ProtectionDomain;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class BlamePlugin implements IMixinConfigPlugin {
-    private static final Logger LOGGER = LogManager.getLogger("BlameLog");
+  private static final Logger LOGGER = LogManager.getLogger("BlameLog");
 
-    static {
-        if (InstrumentationAccess.canInstrument()) {
-            LOGGER.info("[BlameLog] Retranforming Loggers...");
+  static {
+    if (InstrumentationAccess.canInstrument()) {
+      LOGGER.info("[BlameLog] Retranforming Loggers...");
 
-            Class<?> blameUtil;
-            if (AbstractLogger.class.getClassLoader() != BlamePlugin.class.getClassLoader()) {
-                LOGGER.warn("[BlameLog] AbstractLogger and BlamePlugin are on different classloaders!");
-                LOGGER.warn("[BlameLog] This means you're probably running Quilt or Connector. Be ware of issues!");
+      Class<?> blameUtil;
+      if (AbstractLogger.class.getClassLoader() != BlamePlugin.class.getClassLoader()) {
+        LOGGER.warn("[BlameLog] AbstractLogger and BlamePlugin are on different classloaders!");
+        LOGGER.warn(
+            "[BlameLog] This means you're probably running Quilt or Connector. Be ware of issues!");
 
-                try (InputStream stream = BlamePlugin.class.getClassLoader().getResourceAsStream("me/melontini/blamelog/BlameUtil.class")) {
-                    byte[] bytes = MakeSure.notNull(stream, "Can't access BlameUtil.class").readAllBytes();
+        try (InputStream stream = BlamePlugin.class
+            .getClassLoader()
+            .getResourceAsStream("me/melontini/blamelog/BlameUtil.class")) {
+          byte[] bytes =
+              MakeSure.notNull(stream, "Can't access BlameUtil.class").readAllBytes();
 
-                    ClassLoader a = AbstractLogger.class.getClassLoader();
-                    //Thanks Su5eD for the fix. https://github.com/Sinytra/Connector/discussions/12#discussioncomment-6790140
-                    InstrumentationAccess.addReads(AbstractLogger.class.getModule(), a.getUnnamedModule());
+          ClassLoader a = AbstractLogger.class.getClassLoader();
+          // Thanks Su5eD for the fix.
+          // https://github.com/Sinytra/Connector/discussions/12#discussioncomment-6790140
+          InstrumentationAccess.addReads(AbstractLogger.class.getModule(), a.getUnnamedModule());
 
-                    blameUtil = tryDefineClass(a, "me.melontini.blamelog.BlameUtil", bytes, BlameUtil.class.getProtectionDomain());
-                } catch (IOException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                blameUtil = BlameUtil.class;
-            }
+          blameUtil = tryDefineClass(
+              a, "me.melontini.blamelog.BlameUtil", bytes, BlameUtil.class.getProtectionDomain());
+        } catch (IOException
+            | NoSuchMethodException
+            | InvocationTargetException
+            | IllegalAccessException e) {
+          throw new RuntimeException(e);
+        }
+      } else {
+        blameUtil = BlameUtil.class;
+      }
 
-            try {
-                String pattern = "[{simpleClass}#{method}] {message}";
-                Path path = FabricLoader.getInstance().getConfigDir().resolve("blamelog-pattern.txt");
-                if (!Files.exists(path)) {
-                    Files.createDirectories(path.getParent());
-                    Files.writeString(path, pattern);
-                } else {
-                    pattern = Files.readString(path);
-                }
-
-                Field field = blameUtil.getField("pattern");
-                field.setAccessible(true);
-                field.set(null, pattern);
-            } catch (Throwable t) {
-                LOGGER.error("[BlameLog] Failed to set pattern. Using default", t);
-            }
-
-
-            AtomicInteger integer = new AtomicInteger();//Keeping track of the number of methods we've retransformed. Not really necessary, but it's nice to know.
-            try {
-                InstrumentationAccess.retransform(node -> LogPatcher.patch(node, integer),
-                        FabricLoader.getInstance().isDevelopmentEnvironment(),
-                        AbstractLogger.class, Log4jLogger.class);
-            } catch (TransformationException e) {
-                throw new RuntimeException("[BlameLog] Failed to retransform loggers", e);
-            }
-            LOGGER.info("Successfully retransformed {} methods.", integer.get());
+      try {
+        String pattern = "[{simpleClass}#{method}] {message}";
+        Path path = FabricLoader.getInstance().getConfigDir().resolve("blamelog-pattern.txt");
+        if (!Files.exists(path)) {
+          Files.createDirectories(path.getParent());
+          Files.writeString(path, pattern);
         } else {
-            LOGGER.error("[BlameLog] Instrumentation went to get some milk, but never came back...");
+          pattern = Files.readString(path);
         }
+
+        Field field = blameUtil.getField("pattern");
+        field.setAccessible(true);
+        field.set(null, pattern);
+      } catch (Throwable t) {
+        LOGGER.error("[BlameLog] Failed to set pattern. Using default", t);
+      }
+
+      AtomicInteger integer =
+          new AtomicInteger(); // Keeping track of the number of methods we've retransformed. Not
+      // really necessary, but it's nice to know.
+      try {
+        InstrumentationAccess.retransform(
+            node -> LogPatcher.patch(node, integer),
+            FabricLoader.getInstance().isDevelopmentEnvironment(),
+            AbstractLogger.class,
+            Log4jLogger.class);
+      } catch (TransformationException e) {
+        throw new RuntimeException("[BlameLog] Failed to retransform loggers", e);
+      }
+      LOGGER.info("Successfully retransformed {} methods.", integer.get());
+    } else {
+      LOGGER.error("[BlameLog] Instrumentation went to get some milk, but never came back...");
     }
+  }
 
-    private static Method defineClass;
+  private static Method defineClass;
 
-    private static Class<?> tryDefineClass(ClassLoader a, String name, byte[] bytes, ProtectionDomain domain) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        try {
-            return UnsafeUtils.defineClass(a, name, bytes, BlameUtil.class.getProtectionDomain());
-        } catch (Throwable t ) {
-            if (defineClass == null)
-                defineClass = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class, ProtectionDomain.class);
+  private static Class<?> tryDefineClass(
+      ClassLoader a, String name, byte[] bytes, ProtectionDomain domain)
+      throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    try {
+      return UnsafeUtils.defineClass(a, name, bytes, BlameUtil.class.getProtectionDomain());
+    } catch (Throwable t) {
+      if (defineClass == null)
+        defineClass = ClassLoader.class.getDeclaredMethod(
+            "defineClass",
+            String.class,
+            byte[].class,
+            int.class,
+            int.class,
+            ProtectionDomain.class);
 
-            Module javaBase = ClassLoader.class.getModule();
-            if (!defineClass.canAccess(null)) {
-                if (InstrumentationAccess.get().isModifiableModule(javaBase)) {
-                    LOGGER.warn("[BlameLog] Opening java.lang to UNNAMED to make defineClass accessible");
-                    InstrumentationAccess.addOpens(javaBase, Map.of("java.lang", Set.of(BlamePlugin.class.getModule())));
-                    defineClass.setAccessible(true);
-                } else {
-                    LOGGER.warn("[BlameLog] Using unsafe to make defineClass accessible");
-                    Reflect.setAccessible(defineClass);
-                }
-            }
-
-            //Is there a better way to define a class on a different class loader?
-            return (Class<?>) defineClass.invoke(a, name, bytes, 0, bytes.length, domain);
+      Module javaBase = ClassLoader.class.getModule();
+      if (!defineClass.canAccess(null)) {
+        if (InstrumentationAccess.get().isModifiableModule(javaBase)) {
+          LOGGER.warn("[BlameLog] Opening java.lang to UNNAMED to make defineClass accessible");
+          InstrumentationAccess.addOpens(
+              javaBase, Map.of("java.lang", Set.of(BlamePlugin.class.getModule())));
+          defineClass.setAccessible(true);
+        } else {
+          LOGGER.warn("[BlameLog] Using unsafe to make defineClass accessible");
+          Reflect.setAccessible(defineClass);
         }
-    }
+      }
 
-    @Override public void onLoad(String mixinPackage) {}
-    @Override public String getRefMapperConfig() {return null;}
-    @Override public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {return false;}
-    @Override public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
-    @Override public List<String> getMixins() {return null;}
-    @Override public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
-    @Override public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+      // Is there a better way to define a class on a different class loader?
+      return (Class<?>) defineClass.invoke(a, name, bytes, 0, bytes.length, domain);
+    }
+  }
+
+  @Override
+  public void onLoad(String mixinPackage) {}
+
+  @Override
+  public String getRefMapperConfig() {
+    return null;
+  }
+
+  @Override
+  public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+    return false;
+  }
+
+  @Override
+  public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+  @Override
+  public List<String> getMixins() {
+    return null;
+  }
+
+  @Override
+  public void preApply(
+      String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+
+  @Override
+  public void postApply(
+      String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
 }
--- src/main/java/me/melontini/blamelog/BlameUtil.java
+++ src/main/java/me/melontini/blamelog/BlameUtil.java
@@ -1,101 +1,113 @@
 package me.melontini.blamelog;
 
-import org.apache.commons.lang3.StringUtils;
-import org.spongepowered.asm.mixin.transformer.meta.MixinMerged;
+import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 
 import java.lang.reflect.Method;
 import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
-
-import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
+import org.apache.commons.lang3.StringUtils;
+import org.spongepowered.asm.mixin.transformer.meta.MixinMerged;
 
 public class BlameUtil {
-    private static final StackWalker stackWalker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+  private static final StackWalker stackWalker =
+      StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 
-    private static final Map<String, BiFunction<StackWalker.StackFrame, MixinMerged, String>> patterns = Map.of(
-            "{class}", (frame, mixin) -> mixin == null ? frame.getClassName() : mixin.mixin(),
-            "{method}", (frame, mixin) -> frame.getMethodName(),
-            "{simpleClass}", (frame, mixin) -> mixin == null ? simpleClassName(frame.getDeclaringClass()) : simpleClassName(mixin.mixin()),
-            "{methodParams}",  (frame, mixin) -> {
+  private static final Map<String, BiFunction<StackWalker.StackFrame, MixinMerged, String>>
+      patterns = Map.of(
+          "{class}", (frame, mixin) -> mixin == null ? frame.getClassName() : mixin.mixin(),
+          "{method}", (frame, mixin) -> frame.getMethodName(),
+          "{simpleClass}",
+              (frame, mixin) -> mixin == null
+                  ? simpleClassName(frame.getDeclaringClass())
+                  : simpleClassName(mixin.mixin()),
+          "{methodParams}",
+              (frame, mixin) -> {
                 Class<?>[] params = frame.getMethodType().parameterArray();
                 if (params.length == 0) return "";
 
                 StringJoiner joiner = new StringJoiner(",");
                 for (Class<?> param : params) {
-                    joiner.add(simpleClassName(param));
+                  joiner.add(simpleClassName(param));
                 }
                 return joiner.toString();
-            },
-            "{methodReturnType}", (frame, mixin) -> simpleClassName(frame.getMethodType().returnType())
-    );
+              },
+          "{methodReturnType}",
+              (frame, mixin) -> simpleClassName(frame.getMethodType().returnType()));
 
-    private static final List<Predicate<StackWalker.StackFrame>> filters;
+  private static final List<Predicate<StackWalker.StackFrame>> filters;
 
-    static {
-        List<Predicate<StackWalker.StackFrame>> base = new ArrayList<>();
+  static {
+    List<Predicate<StackWalker.StackFrame>> base = new ArrayList<>();
 
-        base.add(frame -> "log".equals(frame.getMethodName()));
+    base.add(frame -> "log".equals(frame.getMethodName()));
 
-        String[] classContains = new String[] {"log4j", "slf4j", "logger"};
-        base.add(frame -> {
-            for (String string : classContains) {
-                if (containsIgnoreCase(frame.getClassName(), string)) return true;
-            }
-            return false;
-        });
+    String[] classContains = new String[] {"log4j", "slf4j", "logger"};
+    base.add(frame -> {
+      for (String string : classContains) {
+        if (containsIgnoreCase(frame.getClassName(), string)) return true;
+      }
+      return false;
+    });
 
-        String[] classEnds = new String[] {"Logger", "Log", "LogHelper", "LoggerAdapterAbstract", "Logging"};
-        base.add(frame -> {
-            for (String string : classEnds) {
-                if (StringUtils.endsWith(frame.getClassName(), string)) return true;
-            }
-            return false;
-        });
+    String[] classEnds =
+        new String[] {"Logger", "Log", "LogHelper", "LoggerAdapterAbstract", "Logging"};
+    base.add(frame -> {
+      for (String string : classEnds) {
+        if (StringUtils.endsWith(frame.getClassName(), string)) return true;
+      }
+      return false;
+    });
 
-        filters = List.copyOf(base);
+    filters = List.copyOf(base);
+  }
+
+  public static String pattern = "[{simpleClass}#{method}] {message}";
+
+  private static String simpleClassName(String cls) {
+    String[] split = cls.split("\\.");
+    return split[split.length - 1];
+  }
+
+  private static String simpleClassName(Class<?> cls) {
+    if (cls.isPrimitive()) return cls.getName();
+    return cls.getName().substring(cls.getPackageName().length() + 1);
+  }
+
+  public static String getMessage(String msg) {
+    StackWalker.StackFrame frame = firstMatching();
+    String methodName = frame.getMethodName();
+
+    MixinMerged mixin = null;
+    if (frame.getClassName().startsWith("net.minecraft")
+        && !StringUtils.equalsAny(methodName, "<init>", "<clinit>")) {
+      var params = frame.getMethodType().parameterArray();
+      for (Method method : frame.getDeclaringClass().getDeclaredMethods()) {
+        if (!method.getName().equals(methodName)) continue;
+        if (method.getParameterCount() != params.length) continue;
+        if (!Arrays.equals(params, method.getParameterTypes())) continue;
+        mixin = method.getAnnotation(MixinMerged.class);
+      }
     }
 
-    public static String pattern = "[{simpleClass}#{method}] {message}";
-
-    private static String simpleClassName(String cls) {
-        String[] split = cls.split("\\.");
-        return split[split.length - 1];
+    String message = pattern;
+    for (Map.Entry<String, BiFunction<StackWalker.StackFrame, MixinMerged, String>> entry :
+        patterns.entrySet()) {
+      message = message.replace(entry.getKey(), entry.getValue().apply(frame, mixin));
     }
+    return message.replace("{message}", msg);
+  }
 
-    private static String simpleClassName(Class<?> cls) {
-        if (cls.isPrimitive()) return cls.getName();
-        return cls.getName().substring(cls.getPackageName().length() + 1);
-    }
-
-    public static String getMessage(String msg) {
-        StackWalker.StackFrame frame = firstMatching();
-        String methodName = frame.getMethodName();
-
-        MixinMerged mixin = null;
-        if (frame.getClassName().startsWith("net.minecraft") && !StringUtils.equalsAny(methodName, "<init>", "<clinit>")) {
-            var params = frame.getMethodType().parameterArray();
-            for (Method method : frame.getDeclaringClass().getDeclaredMethods()) {
-                if (!method.getName().equals(methodName)) continue;
-                if (method.getParameterCount() != params.length) continue;
-                if (!Arrays.equals(params, method.getParameterTypes())) continue;
-                mixin = method.getAnnotation(MixinMerged.class);
-            }
-        }
-
-        String message = pattern;
-        for (Map.Entry<String, BiFunction<StackWalker.StackFrame, MixinMerged, String>> entry : patterns.entrySet()) {
-            message = message.replace(entry.getKey(), entry.getValue().apply(frame, mixin));
-        }
-        return message.replace("{message}", msg);
-    }
-
-    public static StackWalker.StackFrame firstMatching() {
-        return stackWalker.walk(s -> s.skip(3).dropWhile(frame -> {
-            for (Predicate<StackWalker.StackFrame> filter : filters) {
+  public static StackWalker.StackFrame firstMatching() {
+    return stackWalker
+        .walk(s -> s.skip(3)
+            .dropWhile(frame -> {
+              for (Predicate<StackWalker.StackFrame> filter : filters) {
                 if (filter.test(frame)) return true;
-            }
-            return false;
-        }).findFirst()).orElse(null);
-    }
+              }
+              return false;
+            })
+            .findFirst())
+        .orElse(null);
+  }
 }
